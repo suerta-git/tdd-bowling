9ecfef91cfbadc2b0cdbde5ed2d65bf5a6075d4a
Suerta <suerta__@hotmail.com>
2020-09-14 17:37:04 +0800

    [Zhang Jindong] feat: should throw when given null

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class BowlingGame {
+    public int getTotalScore(List<Integer> downPins) {
+        if (downPins == null) {
+            throw new IllegalArgumentException("input should not be null");
+        }
+        return 0;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BowlingGameTest {
+
+    @Test
+    void should_throw_when_given_null() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            BowlingGame bowlingGame = new BowlingGame();
+            int actual = bowlingGame.getTotalScore(null);
+        });
+        assertEquals(exception.getMessage(), "input should not be null");
+    }
 }
